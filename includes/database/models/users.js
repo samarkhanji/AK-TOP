637794e5fabf8b35fde4f9ef0a788a3afe7683d5
aman6ig <aman6ig@outlook.com>
2025-09-08 20:46:29 +0530

    Update users.js

--- includes/database/models/users.js
+++ includes/database/models/users.js
@@ -1,21 +1,61 @@
 module.exports = function({ sequelize, Sequelize }) {
-	let Users = sequelize.define('Users', {
-		num: {
-			type: Sequelize.INTEGER,
-			primaryKey: true,
-			autoIncrement: true
-		},
-		userID: {
-			type: Sequelize.BIGINT,
-			unique: true
-		},
-        name: {
-            type: Sequelize.STRING
+    const Users = sequelize.define('Users', {
+        num: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
         },
-		data: {
-			type: Sequelize.JSON
-		}
-	});
+        userID: {
+            type: Sequelize.BIGINT,
+            unique: true,
+            allowNull: false,
+            validate: {
+                notNull: {
+                    msg: 'User ID is required'
+                },
+                notEmpty: {
+                    msg: 'User ID cannot be empty'
+                }
+            }
+        },
+        name: {
+            type: Sequelize.STRING,
+            allowNull: true,
+            validate: {
+                len: {
+                    args: [1, 255],
+                    msg: 'Name must be between 1 and 255 characters'
+                }
+            }
+        },
+        data: {
+            type: Sequelize.JSON,
+            defaultValue: {},
+            get() {
+                const rawValue = this.getDataValue('data');
+                return rawValue || {};
+            }
+        }
+    }, {
+        indexes: [
+            {
+                unique: true,
+                fields: ['userID']
+            }
+        ],
+        hooks: {
+            beforeCreate: (user, options) => {
+                if (!user.data) {
+                    user.data = {};
+                }
+            },
+            beforeUpdate: (user, options) => {
+                if (!user.data) {
+                    user.data = {};
+                }
+            }
+        }
+    });
 
-	return Users;
-}
+    return Users;
+};

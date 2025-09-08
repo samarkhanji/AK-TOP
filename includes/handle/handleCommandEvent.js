0e1c5da57a8e061037cd0bfdb128081c8d51cce0
aman6ig <aman6ig@outlook.com>
2025-09-08 15:39:35 +0530

    Update handleCommandEvent.js

--- includes/handle/handleCommandEvent.js
+++ includes/handle/handleCommandEvent.js
@@ -1,22 +1,36 @@
 module.exports = function ({ api, models, Users, Threads, Currencies }) {
     const logger = require("../../utils/log.js");
+    
     return function ({ event }) {
         const { allowInbox } = global.config;
         const { userBanned, threadBanned } = global.data;
         const { commands, eventRegistered } = global.client;
+        
         var { senderID, threadID } = event;
         senderID = String(senderID);
         threadID = String(threadID);
-        if (userBanned.has(senderID) || threadBanned.has(threadID) || (allowInbox == false && senderID == threadID)) return;
+        
+        if (userBanned.has(senderID) || 
+            threadBanned.has(threadID) || 
+            (allowInbox == false && senderID == threadID)) {
+            return;
+        }
+        
         for (const eventReg of eventRegistered) {
             const cmd = commands.get(eventReg);
+            if (!cmd || !cmd.handleEvent) continue;
+            
             var getText2;
-
             if (cmd.languages && typeof cmd.languages == 'object') {
                 getText2 = (...values) => {
                     const commandModule = cmd.languages || {};
-                    if (!commandModule.hasOwnProperty(global.config.language)) 
-                        return api.sendMessage(global.getText('handleCommand','notFoundLanguage', cmd.config.name), threadID, messengeID); 
+                    if (!commandModule.hasOwnProperty(global.config.language)) {
+                        return api.sendMessage(
+                            global.getText('handleCommand','notFoundLanguage', cmd.config.name), 
+                            threadID
+                        ); 
+                    }
+                    
                     var lang = cmd.languages[global.config.language][values[0]] || '';
                     for (var i = values.length - 1; i >= 0; i--) {
                         const expReg = RegExp('%' + (i + 1), 'g');
@@ -29,15 +43,17 @@ module.exports = function ({ api, models, Users, Threads, Currencies }) {
             }
 
             try {
-                const Obj = {};
-                Obj.event = event;
-                Obj.api = api;
-                Obj.models = models;
-                Obj.Users = Users;
-                Obj.Threads = Threads;
-                Obj.Currencies = Currencies;
-                Obj.getText = getText2;
-                if (cmd) cmd.handleEvent(Obj);
+                const Obj = {
+                    event: event,
+                    api: api,
+                    models: models,
+                    Users: Users,
+                    Threads: Threads,
+                    Currencies: Currencies,
+                    getText: getText2
+                };
+                
+                cmd.handleEvent(Obj);
             } catch (error) {
                 logger(global.getText('handleCommandEvent', 'moduleError', cmd.config.name), 'error');
             }

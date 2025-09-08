e585a79405f857c1af4c6d3bce897a01ca4c90d4
aman6ig <aman6ig@outlook.com>
2025-09-08 20:45:51 +0530

    Update threads.js

--- includes/database/models/threads.js
+++ includes/database/models/threads.js
@@ -1,21 +1,65 @@
 module.exports = function({ sequelize, Sequelize }) {
-	let Threads = sequelize.define('Threads', {
-		num: {
-			type: Sequelize.INTEGER,
-			primaryKey: true,
-			autoIncrement: true
-		},
-		threadID: {
-			type: Sequelize.BIGINT,
-			unique: true
-		},
-        threadInfo: {
-            type: Sequelize.JSON
+    const Threads = sequelize.define('Threads', {
+        num: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
         },
-		data: {
-			type: Sequelize.JSON
-		}
-	});
+        threadID: {
+            type: Sequelize.BIGINT,
+            unique: true,
+            allowNull: false,
+            validate: {
+                notNull: {
+                    msg: 'Thread ID is required'
+                },
+                notEmpty: {
+                    msg: 'Thread ID cannot be empty'
+                }
+            }
+        },
+        threadInfo: {
+            type: Sequelize.JSON,
+            defaultValue: {},
+            get() {
+                const rawValue = this.getDataValue('threadInfo');
+                return rawValue || {};
+            }
+        },
+        data: {
+            type: Sequelize.JSON,
+            defaultValue: {},
+            get() {
+                const rawValue = this.getDataValue('data');
+                return rawValue || {};
+            }
+        }
+    }, {
+        indexes: [
+            {
+                unique: true,
+                fields: ['threadID']
+            }
+        ],
+        hooks: {
+            beforeCreate: (thread, options) => {
+                if (!thread.threadInfo) {
+                    thread.threadInfo = {};
+                }
+                if (!thread.data) {
+                    thread.data = {};
+                }
+            },
+            beforeUpdate: (thread, options) => {
+                if (!thread.threadInfo) {
+                    thread.threadInfo = {};
+                }
+                if (!thread.data) {
+                    thread.data = {};
+                }
+            }
+        }
+    });
 
-	return Threads;
-}
+    return Threads;
+};
